b424b0702a02c570c8eccb25b207c429e6b551de
IlyaFilippov1 <106549885+IlyaFilippov1@users.noreply.github.com>
2022-12-29 13:31:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace OSeminar7
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            StudentService studentService = new StudentService(
+                new List<Student>() {
+                    new SmallStudent(),
+                    new MiddleStudent(),
+                    new HighStudent(),
+                    new SmallStudent()
+                }
+            );
+            studentService.service();
+        }
+    }
+}
